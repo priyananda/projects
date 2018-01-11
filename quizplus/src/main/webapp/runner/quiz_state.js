e836c805906c0c9092c5caf159cbf1af47035149
Priyananda Shenoy <priyananda@gmail.com>
2018-01-10 23:09:22 -0800

    LocalStorage persistance of quiz state

--- quizplus/src/main/webapp/runner/quiz_state.js
+++ quizplus/src/main/webapp/runner/quiz_state.js
@@ -1,30 +1,55 @@
 quizRunnerModule
-  .factory('QuizState', function($http) {
+  .factory('QuizState', function($http, $window) {
 	 var _currentState = {};
-	 var _teamScores = [
-		{name: "Team 1", points: 0},
-		{name: "Team 2", points: 0},
-		{name: "Team 3", points: 0},
-		{name: "Team 4", points: 0},
-		{name: "Team 5", points: 0},
-		{name: "Team 6", points: 0}
-	  ];
-
+	 var _teamScores = [];
 	 var _questionClosed = [];
-	 for (i = 0; i < questionInfo.lenght; ++i)
-		 _questionClosed.push(false);
 	 
 	 _currentState.getTeamScores = function() {
 		 return _teamScores;
-	 }
+	 };
 	 
 	 _currentState.isQuestionClosed = function(qid) {
 		 return _questionClosed[qid - 1];
-	 }
+	 };
 	 
 	 _currentState.closeQuestion = function(qid) {
 		 _questionClosed[qid - 1] = true;
-	 }
+		 _currentState.saveToLocalStorage();
+	 };
 	 
+	 _currentState.clearLocalStorage = function() {
+		 $window.localStorage.clear();
+	 };
+	 
+	 _currentState.loadFromLocalStorage = function() {
+		 var tsSaved = $window.localStorage.getItem('quiz214-team-scores');
+		 if (tsSaved != null) {
+			 _teamScores = JSON.parse(tsSaved);
+		 } else {
+			 _teamScores = [
+				{name: "Team 1", points: 0},
+				{name: "Team 2", points: 0},
+				{name: "Team 3", points: 0},
+				{name: "Team 4", points: 0},
+				{name: "Team 5", points: 0},
+				{name: "Team 6", points: 0}
+			  ];
+		 }
+		 var qsSaved = $window.localStorage.getItem('quiz214-questions-closed');
+		 if (qsSaved != null) {
+			 _questionClosed = JSON.parse(qsSaved);
+		 } else {
+			 _questionClosed = [];
+			 for (i = 0; i < questionInfo.length; ++i)
+				 _questionClosed.push(false);
+		 }
+	 };
+	 
+	 _currentState.saveToLocalStorage = function() {
+		 $window.localStorage.setItem('quiz214-team-scores', JSON.stringify(_teamScores));
+		 $window.localStorage.setItem('quiz214-questions-closed', JSON.stringify(_questionClosed));
+	 };
+	 
+	 _currentState.loadFromLocalStorage();
 	 return _currentState;
-  })
+  });
--- quizplus/src/main/webapp/runner/scoreboard.js
+++ quizplus/src/main/webapp/runner/scoreboard.js
@@ -1,14 +1,19 @@
 quizRunnerModule
-  .controller('ScoreBoardController', function ($scope) {
+  .controller('ScoreBoardController', function ($scope, QuizState) {
 	ctrl = this;
 	$scope.plusFive = function(teamscore) {
 		teamscore.points += 5;
+		QuizState.saveToLocalStorage();
 	};
 	$scope.minusFive = function(teamscore) {
 		teamscore.points -= 5;
 		if (teamscore.points < 0)
 			teamscore.points = 0;
+		QuizState.saveToLocalStorage();
 	};
+	$scope.clearStorage = function() {
+		QuizState.clearLocalStorage();
+	}
 });
 
 quizRunnerModule.component('scoreboard', {
