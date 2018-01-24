cc2de8e77e2bc6061c5c827084551991dc5113b1
Priyananda Shenoy <priyananda@gmail.com>
2018-01-23 22:47:34 -0800

    Adding final image urls

--- quizplus/src/main/java/us/quizpl/app/QuizMedia.java
+++ quizplus/src/main/java/us/quizpl/app/QuizMedia.java
@@ -9,9 +9,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.apphosting.api.search.DocumentPb.Document.Storage;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import us.quizpl.StorageHelper;
+import us.quizpl.model.Presentation;
+import us.quizpl.model.Slide;
 import us.quizpl.search.QuizResult;
 import us.quizpl.search.QuizRetriever;
 
@@ -31,16 +35,21 @@ public class QuizMedia extends HttpServlet {
 	}
 	
 	private JsonObject getMediaJson(HttpServletRequest request){
-		QuizResult result = QuizRetriever.find(request);
-		ArrayList<String> imageUrls = result.getSlideImageUrls();
+		String presentationId = request.getParameter("id");
+	
+		if (presentationId == null)
+			return null;
 		
+		Presentation presentation = Presentation.getById(Long.parseLong(presentationId));
+		long slideCount = presentation.getSlideCount();
 		JsonArray jsonArray = new JsonArray();
-		for(String url : imageUrls) {
-			jsonArray.add(toJsonObject(url));
+		for (long i = 1; i <= slideCount; ++i) {
+			String servingUrl = StorageHelper.getUrlForGcsFile(Slide.BUCKET_NAME,
+				String.format("%1$s/Slide%2$d.jpg", presentation.getName(), i));
+			jsonArray.add(toJsonObject(servingUrl));
 		}
-		
 		JsonObject jobjOuter = new JsonObject();
-		jobjOuter.add("imageInfo", jsonArray);
+		jobjOuter.add("imageInfo2", jsonArray);
 		return jobjOuter;
 	}
 	
